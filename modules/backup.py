5c0fe97edfd52a40963e26c57964904c450961e5
Christoph <email@ckloth.de>
2021-04-06 21:14:22 +0200

    ensure only new files are backuped, clean script from not neccessary modules

--- modules/backup.py
+++ modules/backup.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
 
-# Ideen:
-# - Videos aufzeichnen, z.B. wenn Bewegung detektiert wird
-# - Favoriten -> eindeutigen Timestamp (date + time)
-# - BACKUP auf mehrere Kameras anpassen!
-# - Similarity Threshold je Kamera separat!
-
-import io, os, time
+import os, time
 import logging
-import json, codecs
-import numpy as np
+import codecs
 import string
 
-import imutils, cv2
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-
 import threading
-from http            import server
 from datetime        import datetime
-
 from modules.camera  import myCamera
 from modules.config  import myConfig
-from modules.presets import myParameters
-from modules.presets import myPages
-
 
 #----------------------------------------------------
 
@@ -54,9 +38,11 @@ class myBackupRestore(threading.Thread):
             backup_started = True
             self.backup_files()
             logging.info("OK.")
+            time.sleep(30)
          else:
             backup_started = False
-         time.sleep(1)
+            
+         time.sleep(30)
 
    def stop(self):
        '''
@@ -173,9 +159,10 @@ class myBackupRestore(threading.Thread):
        directory = self.config.directory(config="images", date=backup_date)
 
        if os.path.isdir(directory):
+         # if the directory but no config file exists for backup directory create a new one
          if not os.path.isfile(self.config.file(config="backup", date=backup_date)):
-           files        = self.compare_files_init(date=backup_date)
-           files_backup = { "files" : {}, "info" : {}}
+           files                             = self.compare_files_init(date=backup_date)
+           files_backup                      = { "files" : {}, "info" : {}}
            files_backup["files"]             = files
            files_backup["info"]["count"]     = len(files)
            files_backup["info"]["threshold"] = {}
@@ -185,6 +172,7 @@ class myBackupRestore(threading.Thread):
            files_backup["info"]["size"]      = sum(os.path.getsize(os.path.join(directory,f)) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory,f)))
            self.config.write(config="backup", config_data=files_backup, date=backup_date)
 
+       # if no directory exists, create directory, copy files and create a new config file (copy existing information)
        else:
          self.config.directory_create(config="images", date=backup_date)
          files        = self.config.read(config="images")
@@ -197,10 +185,10 @@ class myBackupRestore(threading.Thread):
          for cam in self.camera:
            count = 0
            for stamp in stamps:
-             if self.camera[cam].selectImage(timestamp=stamp, file_info=files[stamp]):
 
+             if self.camera[cam].selectImage(timestamp=stamp, file_info=files[stamp]) and files[stamp]["datestamp"] == backup_date:
+               count      += 1
                update_new  = files[stamp]
-
                file_lowres = self.config.imageName(type="lowres", timestamp=stamp, camera=cam)
                file_hires  = self.config.imageName(type="hires",  timestamp=stamp, camera=cam)
 
@@ -214,21 +202,18 @@ class myBackupRestore(threading.Thread):
                  files_backup["files"][stamp] = update_new
 
                  os.popen('cp ' + os.path.join(dir_source,file_lowres) + ' ' + os.path.join(directory,file_lowres))
-                 if os.path.isfile(os.path.join(dir_source,file_hires)):
-                    os.popen('cp ' + os.path.join(dir_source,file_hires)  + ' ' + os.path.join(directory,file_hires))
-
-               count += 1
+                 os.popen('cp ' + os.path.join(dir_source,file_hires)  + ' ' + os.path.join(directory,file_hires))
 
            logging.info(cam + ": " +str(count) + " Bilder gesichert (" + str(self.camera[cam].param["similarity"]["threshold"]) + ")")
 
+         files_backup["info"]["date"]      = backup_date[6:8]+"."+backup_date[4:6]+"."+backup_date[0:4]
          files_backup["info"]["count"]     = count
          files_backup["info"]["size"]      = backup_size
          files_backup["info"]["threshold"] = {}
          for cam in self.camera:
            files_backup["info"]["threshold"][cam] = self.camera[cam].param["similarity"]["threshold"]
-         files_backup["info"]["date"]      = backup_date[6:8]+"."+backup_date[4:6]+"."+backup_date[0:4]
 
-         self.config.write(config="backup",config_data=files_backup,date=directory)
+         self.config.write(config="backup", config_data=files_backup, date=directory)
 
 
    #-----------------------------------
