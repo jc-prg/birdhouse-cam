c5d38e81aa401ef92549dc1ad0fe5e8cb2991d01
Christoph <email@ckloth.de>
2022-03-19 10:34:35 +0100

    srv: add temperature loop

--- temperature.py
+++ temperature.py
@@ -2,13 +2,16 @@
 
 import RPi.GPIO as GPIO
 import modules.dht11 as dht11
+import time
 
 GPIO.setmode(GPIO.BCM)
 sensor = dht11.DHT11(pin=4)
 
-indoor = sensor.read()
-if indoor.is_valid():
+while True:
+  indoor = sensor.read()
+  if indoor.is_valid():
     print("Temperatur: "  + str(indoor.temperature))
     print("Luftfeuchtigkeit: " + str(indoor.humidity))
+  time.sleep(1)
 
 GPIO.cleanup()
