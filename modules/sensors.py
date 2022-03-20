3013613e18016edc838dd5cde6e3c895c013bcb1
Christoph <email@ckloth.de>
2022-03-20 12:49:02 +0100

    srv: cleanup code and add pin as var

--- modules/sensors.py
+++ modules/sensors.py
@@ -7,51 +7,48 @@ import logging
 
 class mySensor(threading.Thread):
 
-   def __init__(self, id, param, config):
-       '''
-       Initialize new thread and set inital parameters
-       '''
-       threading.Thread.__init__(self)
-       self.id           = id
-       self.param        = param
-       self.config       = config
-       self.running      = True
-       self.error        = False
-       self.values       = {}
+    def __init__(self, sensor_id, param, config):
+        """
+        Initialize new thread and set inital parameters
+        """
+        threading.Thread.__init__(self)
+        self.id = sensor_id
+        self.param = param
+        self.config = config
+        self.running = True
+        self.error = False
+        self.pin = 4
+        self.values = {}
 
-       GPIO.setmode(GPIO.BCM)
-       self.sensor = dht11.DHT11(pin=4)
+        GPIO.setmode(GPIO.BCM)
+        self.sensor = dht11.DHT11(pin=self.pin)
 
-       logging.info("Starting sensors ("+self.id+") ...")
+        logging.info("Starting sensors (" + self.id + "/" + str(self.pin) + ") ...")
 
+    def run(self):
+        """
+        Start recording from sensors
+        """
+        while self.running and not self.error:
+            try:
+                indoor = self.sensor.read()
+                if indoor.is_valid():
+                    self.values["temperature"] = indoor.temperature
+                    self.values["humidity"] = indoor.humidity
+                    logging.debug("Temperature: " + str(indoor.temperature))
+                    logging.debug("Humidity:    " + str(indoor.humidity))
+                else:
+                    logging.warning("Could not read data from sensor '" + self.id + "': " + str(indoor.error_code))
+            except Exception as e:
+                logging.warning("Error reading data from sensor '" + self.id + "'")
 
-   def run(self):
-       '''
-       Start recording from sensors
-       '''
-       while (self.running and not self.error):
-         try:
-           indoor = self.sensor.read()
-           if indoor.is_valid():
-             self.values["temperature"] = indoor.temperature
-             self.values["humidity"]    = indoor.humidity
-             logging.debug("Temperature: " + str(indoor.temperature))
-             logging.debug("Humidity:    " + str(indoor.humidity))
-           else:
-             logging.warning("Could not read data from sensor '"+self.id+"': "+str(indoor.error_code))
-         except Exception as e:
-           logging.warning("Error reading data from sensor '"+self.id+"'")
-           
-         time.sleep(1)
-       
-       logging.info("Stopped sensors ("+self.id+").")
+            time.sleep(1)
 
+        logging.info("Stopped sensors (" + self.id + ").")
 
-   def stop(self):
-       '''
-       Stop sensors
-       '''
-       GPIO.cleanup()
-       self.running = False
-
-
+    def stop(self):
+        """
+        Stop sensors
+        """
+        GPIO.cleanup()
+        self.running = False
