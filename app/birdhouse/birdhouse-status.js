e9de8f0b47051289daa27b44efd82c37e99cdca2
Christoph <email@ckloth.de>
2022-03-24 23:37:45 +0100

    app: format temperature info

--- app/birdhouse/birdhouse-status.js
+++ app/birdhouse/birdhouse-status.js
@@ -11,22 +11,14 @@ function birdhouseStatus_print(data) {
 
     var sensors = data["DATA"]["sensors"];
     html = "<center><i><font color='gray'>";
-    html += "Temperature (";
+    html += "Temperature - ";
     var count = 0;
     var keys = Object.keys(sensors);
     for (let sensor in sensors) {
         if (sensors[sensor]["active"]) {
-            html += sensors[sensor]["name"];
-            count += 1;
-            if (count < keys.length) { html += " / "; }
-        }
-    }
-    html += "): ";
-    var count = 0;
-    for (let sensor in sensors) {
-        if (sensors[sensor]["active"]) {
+            html += sensors[sensor]["name"] + ": ";
             html += "<font id='temp"+sensor+"'>"+sensors[sensor]["values"]["temperature"]+"</font>";
-            html += sensors[sensor]["units"]["temperature"]+" ";
+            html += sensors[sensor]["units"]["temperature"];
             count += 1;
             if (count < keys.length) { html += " / "; }
         }
