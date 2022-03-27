f8ee45c7eba2ea69e09a7b6b53539b8e02e7073e
Christoph <email@ckloth.de>
2022-03-27 22:41:55 +0200

    app: remove micro switches from index page

--- app/birdhouse/birdhouse-views.js
+++ app/birdhouse/birdhouse-views.js
@@ -47,11 +47,13 @@ function birdhouse_INDEX(data, camera) {
 		var onclick  = "birdhousePrint_load(view=\"TODAY\", camera=\""+active_camera+"\");";
 		html += birdhouse_Camera(main=true, view="cam1", onclick=onclick, camera=active_cam, stream_server=stream_server, admin_allowed=admin_allowed);
 		html += "<br/>&nbsp;<br/>";
+		/*
 		for (let micro in birdhouseMicrophones) {
 		    if (birdhouseMicrophones[micro]["active"]) {
 		        html += birdhouseStream_toggle_image(micro);
 		    }
 		}
+		*/
 		app_camera_source[active_cam["name"]] = stream_server + cameras[active_cam["name"]]["stream"];
     }
 	else {
@@ -62,11 +64,13 @@ function birdhouse_INDEX(data, camera) {
 		onclick = "birdhousePrint_load(view=\"TODAY\", camera=\""+active_camera+"\");";
 		html += birdhouse_Camera(main=true, view="cam1cam2", onclick=onclick, camera=active_cam, stream_server=stream_server, admin_allowed=admin_allowed);
 		app_camera_source[active_cam["name"]] = stream_server + cameras[active_cam["name"]]["stream"];
+		/*
 		for (let micro in birdhouseMicrophones) {
 		    if (birdhouseMicrophones[micro]["active"]) {
 		        html += birdhouseStream_toggle_image(micro);
 		    }
 		}
+		*/
 	}
 
 	setTextById(app_frame_content,html);
