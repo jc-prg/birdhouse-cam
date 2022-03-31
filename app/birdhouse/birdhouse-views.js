9976a8444aa61a147c5431fcf6375fb3874a0739
Christoph <email@ckloth.de>
2022-03-31 15:43:41 +0200

    app: check if cameras available

--- app/birdhouse/birdhouse-views.js
+++ app/birdhouse/birdhouse-views.js
@@ -40,6 +40,11 @@ function birdhouse_INDEX(data, camera) {
             }
 		}
 	if (active_cam == {} && other_cams != []) { active_cam = other_cams[0]; other_cams.shift(); }
+
+	if (Object.keys(cameras).length == 0) {
+	    html += lang("NO_ENTRIES");
+	    console.log(cameras);
+	}
 	if (Object.keys(cameras).length == 1 || other_cams.length == 0) {
 		var onclick  = "birdhousePrint_load(view=\"TODAY\", camera=\""+active_camera+"\");";
 		html += birdhouse_Camera(main=true, view="cam1", onclick=onclick, camera=active_cam, stream_server=stream_server, admin_allowed=admin_allowed);
