6d81ab01034991a1b3251dd6a58c83e328b62e7c
Christoph <email@ckloth.de>
2022-03-31 16:31:37 +0200

    app: bugfix sensors

--- app/birdhouse/birdhouse-views.js
+++ app/birdhouse/birdhouse-views.js
@@ -193,7 +193,9 @@ function birdhouse_LIST(title, data, camera, header_open=true) {
         for (var x=1;x<chart_data["titles"].length;x++) {
             if (chart_data["titles"][x].indexOf(":")>-1) {
                 var sensor = chart_data["titles"][x].split(":");
-                chart_titles.push(sensor[1].charAt(0).toUpperCase()+sensor[1].slice(1)+" ("+sensors[sensor[0]]["name"]+")");
+                chart_titles.push(sensor[1].charAt(0).toUpperCase()+sensor[1].slice(1);
+                if (sensors[sensor[0]) { " ("+sensors[sensor[0]]["name"]+")"); }
+                else { console.log(sensor[0]); console.log(sensors); }
             }
         }
         var chart = birdhouseChart_create(title=chart_titles,data=chart_data["data"]);
