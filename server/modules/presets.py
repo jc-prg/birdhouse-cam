c62cf47e7ca7126b1e40ed139999479ed235f409
Christoph <email@ckloth.de>
2022-04-02 22:04:09 +0200

    srv: optimize presets for initial configuration

--- server/modules/presets.py
+++ server/modules/presets.py
@@ -10,8 +10,94 @@ birdhouse_pages = {
     "videos":           ("Videos",      "/videos.html",      "VIDEOS"),
     "save":             ("Speichern",   "/image.jpg",        "")
 }
+
+birdhouse_default_cam_pi = {
+    "type": "pi",
+    "name": "Inside",
+    "source": 1,
+    "active": True,
+    "record": True,
+    "similarity": {
+        "threshold": 95,
+        "detection_area": (0.05, 0.1, 0.95, 0.95)
+    },
+    "image": {
+        "crop": (0.1, 0.0, 0.9, 1.0),
+        "framerate": 24,
+        "resolution": "900x1080",
+        "saturation": -50,
+        "rotation": 180,
+        "preview_scale": 18,
+        "date_time": True,
+        "date_time_position": (10, 20),
+        "date_time_color": (255, 255, 255),
+        "date_time_size": 0.4
+    },
+    "image_save": {
+        "path": "images",
+        "color": "GRAY",
+        "seconds": ("00", "20", "40"),
+        "hours": ("06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
+    },
+    "video": {
+        "allow_recording": True,
+        "max_length": 180,
+        "stream_port": 8008
+    }
+}
+birdhouse_default_cam_usb = {
+    "type": "usb",
+    "name": "Outside",
+    "source": 0,
+    "active": True,
+    "record": True,
+    "similarity": {
+        "threshold": 90,
+        "detection_area": (0.1, 0.1, 0.8, 0.8)
+    },
+    "image": {
+        "crop": (0.1, 0.0, 0.85, 1.0),
+        "resolution": "640x480",
+        "framerate": "not implemented",
+        "saturation": "not implemented",
+        "rotation": 0,
+        "preview_scale": 18,
+        "date_time": True,
+        "date_time_position": (10, 20),
+        "date_time_color": (255, 255, 255),
+        "date_time_size": 0.4
+    },
+    "image_save": {
+        "path": "images",
+        "color": "ORIGINAL",
+        "seconds": ("10", "30", "50"),
+        "hours": ("06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
+    },
+    "video": {
+        "allow_recording": True,
+        "max_length": 180,
+        "stream_port": 8008
+    }
+}
+birdhouse_default_micro = {
+    "active": True,
+    "name": "Inside",
+    "type": "usb",
+    "port": 5002
+}
+birdhouse_default_sensor = {
+    "active": True,
+    "name": "Inside",
+    "type": "dht11",
+    "pin": 4,
+    "units": {
+        "temperature": "°C",
+        "humidity": "%"
+    }
+}
+
 birdhouse_preset = {
-    "title": "Unser Nistkasten :-)",
+    "title": "Our Birdhouse :-)",
     "backup": {
         "preview":   "0700",               # HHMM
         "time":      "2000"
@@ -27,102 +113,14 @@ birdhouse_preset = {
     },
     "devices": {
         "cameras": {
-            "cam1": {
-                "type":     "pi",
-                "name":     "Innen",
-                "source":   1,
-                "active":   True,
-                "record":   True,
-                "similarity": {
-                    "threshold":        95,
-                    "detection_area":   (0.05,0.1,0.95,0.95)
-                },
-                "image": {
-                    "crop": (0.1, 0.0, 0.9, 1.0),
-                    "framerate": 24,
-                    "resolution": "900x1080",
-                    "saturation": -50,
-                    "rotation": 180,
-                    "preview_scale": 18,
-                    "date_time": True,
-                    "date_time_position": (10, 20),
-                    "date_time_color": (255, 255, 255),
-                    "date_time_size": 0.4
-                },
-                "image_save": {
-                    "path":     "images",
-                    "color":    "GRAY",
-                    "seconds":  ("00", "20", "40"),
-                    "hours":    ("06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
-                },
-                "video": {
-                    "allow_recording":  True,
-                    "max_length":       180,
-                    "stream_port":      8008,
-                    "streaming_server": "http://192.168.1.20:8008/"
-                }
-            },
-            "cam2": {
-                "type":     "usb",
-                "name":     "Au&szlig;en",
-                "source":   0,
-                "active":   True,
-                "record":   True,
-                "similarity": {
-                    "threshold":        90,
-                    "detection_area":   (0.1, 0.1, 0.8, 0.8)
-                },
-                "image": {
-                    "crop":         (0.1, 0.0, 0.85, 1.0),
-                    "resolution":   "not implemented (640x480)",
-                    "framerate":    "not implemented",
-                    "saturation":   "not implemented",
-                    "rotation":     "not implemented",
-                    "preview_scale": 18,
-                    "date_time": True,
-                    "date_time_position": (10, 20),
-                    "date_time_color": (255, 255, 255),
-                    "date_time_size": 0.4
-                },
-                "image_save": {
-                    "path":     "images",
-                    "color":    "ORIGINAL",
-                    "seconds":  ("10", "30", "50"),
-                    "hours":    ("06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
-                },
-                "video": {
-                    "allow_recording":      True,
-                    "max_length":           180,
-                    "stream_port":          8008,
-                    "streaming_server":     "http://192.168.1.20:8008/"
-                }
-            }
+            "cam1": birdhouse_default_cam_pi,
+            "cam2": birdhouse_default_cam_usb
         },
         "microphones": {
-            "mic1": {
-                "active": True,
-                "name": "Innen",
-                "type": "usb",
-                "port": 5002
-            },
-            "mic2" : {
-                "active": False,
-                "name": "Außen",
-                "type": "usb",
-                "port": 5001
-            }
+            "mic1": birdhouse_default_micro
         },
         "sensors": {
-            "sensor1": {
-                "active": True,
-                "name": "Innen",
-                "type": "dht11",
-                "pin": 4,
-                "units": {
-                    "temperature": "°C",
-                    "humidity": "%"
-                }
-            }
+            "sensor1": birdhouse_default_sensor
         }
     }
 }
