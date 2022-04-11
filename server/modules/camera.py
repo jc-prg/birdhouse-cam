8afc2af299f450240972db2d0431cc2f14a5aad7
Christoph <email@ckloth.de>
2022-04-11 15:58:24 +0200

    srv: set resolution ... debugging

--- server/modules/camera.py
+++ server/modules/camera.py
@@ -1166,7 +1166,7 @@ class BirdhouseCamera(threading.Thread):
                 else:
                     if "x" in self.param["image"]["resolution"]:
                         resolution = self.param["image"]["resolution"].split("x")
-                        current = [self.camera.stream.get(cv2.CAP_PROP_FRAME_WIDTH ,self.camera.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)]
+                        current = [self.camera.stream.get(cv2.CAP_PROP_FRAME_WIDTH) ,self.camera.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)]
                         logging.info("Current resolution: "+str(current))
                         logging.info("Set resolution: "+str(resolution))
                         # self.camera.stream.set(3, int(resolution[0]))
