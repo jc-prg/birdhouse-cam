da3756101e9bd0b0ed6bf7b09cf40f6d1ec4acb9
Christoph <email@ckloth.de>
2023-02-07 16:38:43 +0100

    srv: add max resolution to API

--- server/server.py
+++ server/server.py
@@ -371,6 +371,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     camera_data[key]["video"]["stream"] = "/stream.mjpg?" + key
                     camera_data[key]["video"]["stream_detect"] = "/detection/stream.mjpg?" + key
                     camera_data[key]["device"] = "camera"
+                    camera_data[key]["image"]["resolution_max"] = camera[key].max_resolution
                     camera_data[key]["status"] = {
                         "error": camera[key].error,
                         "error_warn": camera[key].error_image,
