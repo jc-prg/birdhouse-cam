11cbcd0ec396fa6afc44a2f41a0ef84e8a85d6c7
Christoph <email@ckloth.de>
2023-02-24 23:04:18 +0100

    srv: bugfix chart data

--- server/modules/views.py
+++ server/modules/views.py
@@ -79,9 +79,10 @@ def create_chart_data(data, config=None):
         if "sensor" in data[key]:
             for sensor in data[key]["sensor"]:
                 for sensor_key in data[key]["sensor"][sensor]:
-                    sensor_title = sensor + ":" + sensor_key
-                    if sensor_title not in chart["titles"]:
-                        chart["titles"].append(sensor_title)
+                    if sensor_key != "date":
+                        sensor_title = sensor + ":" + sensor_key
+                        if sensor_title not in chart["titles"]:
+                            chart["titles"].append(sensor_title)
 
         if "weather" in data[key]:
             if "location" in data[key]["weather"]:
