567893f66eb2f9e89bd068ec004a2a1555b03655
Christoph <email@ckloth.de>
2023-12-10 08:44:35 +0100

    srv: bugfix calculate dir size

--- server/modules/views.py
+++ server/modules/views.py
@@ -1156,7 +1156,7 @@ class BirdhouseViews(threading.Thread, BirdhouseClass):
                     archive_changed[cam]["chart_data"] = {"data": {}, "titles": ["Activity"], "info": "not implemented"}
 
             for cam in self.camera:
-                self.archive_changed[cam]["entries"][date]["dir_size"] = dir_size_date
+                archive_changed[cam]["entries"][date]["dir_size"] = dir_size_date
                 self.archive_views[cam] = archive_changed[cam].copy()
 
             # stop if shutdown signal was send
