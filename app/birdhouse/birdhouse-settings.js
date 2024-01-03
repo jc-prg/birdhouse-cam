74687bf094efa742e8c7d554104aa638e8c153f5
Christoph <email@ckloth.de>
2024-01-03 15:39:42 +0100

    srv: cleanup html

--- app/birdhouse/birdhouse-settings.js
+++ app/birdhouse/birdhouse-settings.js
@@ -192,25 +192,25 @@ function birdhouse_app_settings (name="Settings") {
         api_call   += "<button onclick='window.open(\"" + RESTurl + "api/no-id/INDEX/\",\"_blank\");' class='button-settings-api';>INDEX</button>";
         html_entry += this.tab.row("API Calls", api_call);
 
-        api_call    = "<button onclick='birdhouse_forceBackup();' class='button-settings-api';>Force Backup</button>";
-        api_call   += "<button onclick='birdhouse_forceRestart();' class='button-settings-api';>Force Restart</button>";
-        api_call   += "<button onclick='birdhouse_forceUpdateViews();' class='button-settings-api';>Update Views</button>";
-        api_call   += "<button onclick='birdhouse_forceUpdateViews(true);' class='button-settings-api';>Update Views Complete</button>";
-        api_call   += "<button onclick='birdhouse_recreateImageConfig();' class='button-settings-api';>NewImgCfg</button>";
-        api_call   += "<button onclick='birdhouse_removeDataToday();' class='button-settings-api';>CleanAllToday</button>";
-        api_call   += "<button onclick='birdhouse_checkTimeout();' class='button-settings-api';>Timeout</button>";
+        api_call    = "<button onclick='birdhouse_forceBackup();' class='button-settings-api'>Force Backup</button>";
+        api_call   += "<button onclick='birdhouse_forceRestart();' class='button-settings-api'>Force Restart</button>";
+        api_call   += "<button onclick='birdhouse_forceUpdateViews();' class='button-settings-api'>Update Views</button>";
+        api_call   += "<button onclick='birdhouse_forceUpdateViews(true);' class='button-settings-api'>Update Views Complete</button>";
+        api_call   += "<button onclick='birdhouse_recreateImageConfig();' class='button-settings-api'>NewImgCfg</button>";
+        api_call   += "<button onclick='birdhouse_removeDataToday();' class='button-settings-api'>CleanAllToday</button>";
+        api_call   += "<button onclick='birdhouse_checkTimeout();' class='button-settings-api'>Timeout</button>";
         html_entry += this.tab.row("API Commands", api_call);
 
 	    for (let camera in cameras) {
-	        api_call  = "<button onclick='window.open(\"" + RESTurl + "api/no-id/TODAY/"+camera+"/\",\"_blank\");' class='button-settings-api';>Today "+camera.toUpperCase()+"</button>";
-	        api_call += "<button onclick='window.open(\"" + RESTurl + "api/no-id/TODAY_COMPLETE/"+camera+"/\",\"_blank\");' class='button-settings-api';>Compl. "+camera.toUpperCase()+"</button>";
-	        api_call += "<button onclick='window.open(\"" + RESTurl + "api/no-id/ARCHIVE/"+camera+"/\",\"_blank\");' class='button-settings-api';>Archive "+camera.toUpperCase()+"</button>";
+	        api_call  = "<button onclick='window.open(\"" + RESTurl + "api/no-id/TODAY/"+camera+"/\",\"_blank\");' class='button-settings-api'>Today "+camera.toUpperCase()+"</button>";
+	        api_call += "<button onclick='window.open(\"" + RESTurl + "api/no-id/TODAY_COMPLETE/"+camera+"/\",\"_blank\");' class='button-settings-api'>Compl. "+camera.toUpperCase()+"</button>";
+	        api_call += "<button onclick='window.open(\"" + RESTurl + "api/no-id/ARCHIVE/"+camera+"/\",\"_blank\");' class='button-settings-api'>Archive "+camera.toUpperCase()+"</button>";
             html_entry += this.tab.row("API "+camera, api_call);
         }
 
         for (let micro in microphones) {
-            api_call    = "<button onclick='birdhouse_recordStartAudio(\""+micro+"\");' class='button-settings-api';>Record "+micro+"</button>";
-            api_call   += "<button onclick='birdhouse_recordStopAudio(\""+micro+"\");' class='button-settings-api';>Stop "+micro+"</button>";
+            api_call    = "<button onclick='birdhouse_recordStartAudio(\""+micro+"\");' class='button-settings-api'>Record "+micro+"</button>";
+            api_call   += "<button onclick='birdhouse_recordStopAudio(\""+micro+"\");' class='button-settings-api'>Stop "+micro+"</button>";
             html_entry += this.tab.row("API "+micro, api_call);
         }
         html_entry += this.tab.end();
