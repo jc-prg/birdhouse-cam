997ab1913f80b02375bc03b89ceee76c8b74d5f2
Christoph <email@ckloth.de>
2024-01-24 18:03:09 +0100

    app: adapt labels in birds view

--- app/birdhouse/birdhouse-objects.js
+++ app/birdhouse/birdhouse-objects.js
@@ -56,7 +56,7 @@ function birdhouse_OBJECTS(title, data) {
         var entry_information = "";
 
         if (value["detections"]["favorite"] > 0 ) {
-            favorite_label += "<div class='other_label'>&nbsp;" + lang("FAVORITES") + "&nbsp;(" + value["detections"]["favorite"] + ")&nbsp;</div><hr/>";
+            favorite_label += "<div class='other_label'>&nbsp;" + lang("FAVORITES") + "&nbsp;(" + value["detections"]["favorite"] + ")&nbsp;</div>";
             }
         else  { value["detections"]["favorite"] = 0; }
 
