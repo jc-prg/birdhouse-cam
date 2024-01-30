127f48dc486d72bf57b94f48d26a46baeb2a14c7
Christoph <email@ckloth.de>
2024-01-30 17:09:46 +0100

    app: first step to collect image ids for download

--- app/birdhouse/birdhouse-downloads.js
+++ app/birdhouse/birdhouse-downloads.js
@@ -1,14 +1,31 @@
+//--------------------------------------
+// jc://birdhouse/, (c) Christoph Kloth
+//--------------------------------------
+// functions to collect images to be downloaded
+//--------------------------------------
 
+/*
+* activate collecting images for download
+*/
 function collect4download_activate() {
     app_collect4download = true;
     birdhouseReloadView();
 }
 
+/*
+* deactivate collecting images for download
+*/
 function collect4download_deactivate() {
     app_collect4download = false;
     birdhouseReloadView();
 }
 
+/*
+* check if image has been collected already
+*
+* @params (string) entry - entry in the format YYYYMMDD_HHMMSS
+* @returns (string) - true if file already has been collected
+*/
 function collect4download_exists(entry) {
     if (app_collect_list.includes(entry)) { return true; }
     else                                  { return false; }
@@ -21,24 +38,25 @@ function collect4download_exists(entry) {
 * @params (string) image_id - id of the image element, format 'c_' + image_id; image will be replaced depending on status
 */
 function collect4download_toggle(entry, image_id) {
-    var image = "";
-    var img_dir     = "birdhouse/img/";
-
     if (collect4download_exists(entry)) {
         var index = app_collect_list.indexOf(entry);
         if (index != -1) { app_collect_list.splice(index, 1); }
-        image = img_dir + "checkbox-white0.png";
         }
     else {
         app_collect_list.push(entry);
-        image = img_dir + "checkbox-white1.png";
         }
     img_object = document.getElementById("cb_"+image_id);
-    img_object.src = image;
+    img_object.src = collect4download_image(entry);
 }
 
+/*
+* get image path depending if image has been marked or not
+*
+* @params (string) entry - entry in the format YYYYMMDD_HHMMSS
+* @returns (string) - path of checkbox image (with or without check mark)
+*/
 function collect4download_image(entry) {
     var img_dir     = "birdhouse/img/";
-    if (collect4download_exists(entry)) { return img_dir + "checkbox-white1.png"; }
+    if (collect4download_exists(entry)) { return img_dir + "checkbox-white1b.png"; }
     else                                { return img_dir + "checkbox-white0.png"; }
 }
