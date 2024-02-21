b84fc1ea35e5fc2811a7511eeccbd7bb827f96d0
Christoph <email@ckloth.de>
2024-02-21 21:58:56 +0100

    logging information

--- server/modules/camera.py
+++ server/modules/camera.py
@@ -2224,7 +2224,7 @@ class BirdhouseCamera(threading.Thread, BirdhouseCameraClass):
                 self.logging.info(camera_string + " - ERROR: " + str(system["video_devices_complete"][key]["error"]))
                 birdhouse_initial_connect_msg[key] += ", error='" + str(system["video_devices_complete"][key]["error"]) + "'"
             else:
-                self.logging.warning(camera_string + " - OK")
+                self.logging.info(camera_string + " - OK")
 
         self.available_devices = system
         return system
