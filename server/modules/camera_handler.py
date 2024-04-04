53519f0250655d4b409e521a1d3437d0d3bd53fe
Christoph <email@ckloth.de>
2024-04-04 16:22:39 +0200

    srv: add debugging information

--- server/modules/camera_handler.py
+++ server/modules/camera_handler.py
@@ -483,7 +483,7 @@ class BirdhousePiCameraHandler(BirdhouseCameraClass):
             bool: status setting camera resolution
         """
         try:
-            self.configuration["main"]["size"] = (int(width), int(height))
+            #self.configuration["main"]["size"] = (int(width), int(height))
             self.configuration["raw"]["size"] = (int(width), int(height))
             self.logging.debug("Set resolution: " + str(self.configuration["main"]["size"]))
             self.stream.stop()
