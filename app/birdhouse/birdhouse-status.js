4d0c120de3b27481846e6f521126d326c2c8ac4c
Christoph <email@ckloth.de>
2025-03-06 21:01:41 +0100

    looking for bug kill stream

--- app/birdhouse/birdhouse-status.js
+++ app/birdhouse/birdhouse-status.js
@@ -87,7 +87,7 @@ function birdhouseStatus_print(data) {
     var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
     if (height > 1.5 * document.body.clientHeight) { elementVisible("move_up"); }
     else { elementHidden("move_up"); }
-    if (appSettings.active) { setTextById("device_status_short", birdhouseDevices("", data, "short")); } // !!!! Quelle des Problems? Zu oft aufgerufen?
+    //if (appSettings.active) { setTextById("device_status_short", birdhouseDevices("", data, "short")); } // !!!! Quelle des Problems? Zu oft aufgerufen?
                                                                                                          // bezieht sich auf die Settings
     birdhouseStatus_system(data);
     birdhouseStatus_cameras(data);
