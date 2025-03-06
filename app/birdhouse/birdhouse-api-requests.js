ccf44a99424b344ca48e4bf5bba15fa723e887be
Christoph <email@ckloth.de>
2025-03-06 21:28:40 +0100

    fixed bug kill stream

--- app/birdhouse/birdhouse-api-requests.js
+++ app/birdhouse/birdhouse-api-requests.js
@@ -309,6 +309,7 @@ function birdhouse_killStream(camera_id, stream_id) {
     camera_id = camera_id.replace("_img", "");
     stream_id = stream_id.replace("_img", "");
 	commands = ["kill-stream", stream_id, camera_id];
+	birdhouse_active_video_streams[data["kill-stream-id"]] = false;
 	birdhouse_apiRequest('POST', commands, '', birdhouse_killStreamAnswer, '', 'birdhouse_killStream');
     }
 
