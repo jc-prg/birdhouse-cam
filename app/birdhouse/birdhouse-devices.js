2cd7b31901395a25ceb2687f58674617e9263627
Christoph <email@ckloth.de>
2025-03-14 16:15:30 +0100

    reengineering of camera controls for picamera

--- app/birdhouse/birdhouse-devices.js
+++ app/birdhouse/birdhouse-devices.js
@@ -721,7 +721,7 @@ function birdhouseDevices_cameraSettings (data) {
             this_camera_type = "old";
             }
 
-        Object.entries(this_camera_properties.forEach(([key,value]) => {
+        Object.entries(this_camera_properties).forEach(([key,value]) => {
             if (value[1].indexOf("w") >= 0)      { camera_settings_write.push(key); }
             else if (value[1].indexOf("r") >= 0) { camera_settings_read.push(key); }
             if (value[1].indexOf("m") >= 0)      { camera_settings_measure.push(key); }
