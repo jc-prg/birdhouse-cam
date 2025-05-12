df983fd4436b5f029189bf0dc0c1cb8b3246f0ed
Christoph <email@ckloth.de>
2025-05-12 21:57:57 +0200

    debugging microphone control

--- server/modules/bh_ffmpeg.py
+++ server/modules/bh_ffmpeg.py
@@ -33,8 +33,8 @@ class BirdhouseFfmpegTranscoding(BirdhouseClass):
         self.camera_config = config.param["devices"]["cameras"][self.camera_id]
         self.micro_id = config.param["devices"]["cameras"][self.camera_id]["record_micro"]
         self.micro_config = {}
-        if self.micro_id != "" and self.micro_id in config.param["devices"]["micros"]:
-            self.micro_config = self.camera_config["devices"]["micros"][self.micro_id]
+        if self.micro_id != "" and self.micro_id in config.param["devices"]["microphones"]:
+            self.micro_config = self.camera_config["devices"]["microphones"][self.micro_id]
             self.audio_samplerate = str(self.micro_config["sample_rate"])
 
         self.logging.info("Connect ffmpeg for '" + self.camera_id + "' / '" + self.micro_id + "'")
